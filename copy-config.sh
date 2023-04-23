8729074461f184a6ee2e5db1e1573ce89fd27520
Filpill <filiplivancic@gmail.com>
2023-04-23 18:12:30 +0000

    updating config files Sun 23 Apr 18:12:30 UTC 2023

--- copy-config.sh
+++ copy-config.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 dotFolder="dotfiles/"
 confFolder="config/"
+shellFolder="shell/"
 
-echo "--------------------------------"
-echo "     Saving dotfiles files"
-echo "--------------------------------"
+echo "--------------------------------------------------------------"
+echo "                       Saving dotfiles"
+echo "--------------------------------------------------------------"
 cp -v ~/.zshrc 				${dotFolder}.zshrc
 cp -v ~/.zprofile 			${dotFolder}.zprofile
 cp -v ~/.vimrc 			        ${dotFolder}.vimrc
 cp -v ~/.xinitrc 			${dotFolder}.xinitrc
 
-echo "--------------------------------"
-echo "      Saving config files"
-echo "--------------------------------"
+echo "--------------------------------------------------------------"
+echo "                     Saving config files"
+echo "--------------------------------------------------------------"
 cp -r -v ~/.config/picom/ 		${confFolder}
 cp -r -v ~/.config/ranger/		${confFolder}
+
+echo "--------------------------------------------------------------"
+echo "                     Saving shell scripts"
+echo "--------------------------------------------------------------"
+cp -v ~/.local/bin/random_wallpaper.sh 	${shellFolder}random_wallpaper.sh
+
