14824ce9c13c550e19ce9e45b6852dd8a6b13421
Filpill <filiplivancic@gmail.com>
2024-03-15 14:42:44 +0000

    remapped dmenu wallpaper folder

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-folder="$HOME/desktop_setup/wallpaper"
+folder="$HOME/desktop_setup/wallpaperDL/screensaver"
 xwallpaper --stretch $folder/$(find $folder -type f -exec basename {} \; | sort | dmenu)
